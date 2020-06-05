42e67a49f74ecf45ba6bf2bbe17b4b8ff8046b37
InesAzougado <57488512+InesAzougado@users.noreply.github.com>
2020-06-05 10:18:36 +0100

    alteracoes

--- pfai2/src/App.js
+++ pfai2/src/App.js
@@ -2,6 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const express=require('express');
+const app=express();
+//Configurações
+app.set('port', process.env.PORT||3000);
+//Middlewares
+app.use(express.json());
+//Rotas
+app.use('/teste',(req,res)=>{
+  res.send("Rota TESTE.");
+});
+app.use('/',(req,res)=>{
+  res.send("Hello World");
+});
+app.listen(app.get('port'),()=>{
+  console.log("Start server on port " + app.get('port'))
+})
+
 function App() {
   return (
     <div className="App">
