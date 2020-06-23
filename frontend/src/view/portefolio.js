f5eb5f68f5d3e89652e310a5cb2460622cb796f6
InesAzougado <57488512+InesAzougado@users.noreply.github.com>
2020-06-23 11:07:38 +0100

    layout quase feito

--- frontend/src/view/portefolio.js
+++ frontend/src/view/portefolio.js
@@ -47,8 +47,115 @@ class EditComponent extends React.Component {
     }
     render() {
         return (
-            <div>
-                
+            <div class="vernizesscroll">
+                <div class="centerscroll">
+                    <div class="correcao2" class="container-fluid">
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-12 col-md-12 col-lg-12">
+                                <div class="conteudodiv" id="1">
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="cardimgportfolio">
+                                            <div class="overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
