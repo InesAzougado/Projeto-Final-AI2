b01b13b2b46387dbbea1cee5a6b75a24961c9cf0
InesAzougado <57488512+InesAzougado@users.noreply.github.com>
2020-07-11 15:24:28 +0100

    home

---
+++ frontend/src/view/inicio.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../App.css';
+import { data } from 'jquery';
+import swal from 'sweetalert2';
+import axios from 'axios';
+
+window.Swal = swal;
+
+class EditComponent extends React.Component {
+
+    render() {
+        return (
+            <div class="logincenter">
+                
+                 <h1>BEM VINDO!!!!</h1>
+
+            </div>
+        );
+    }
+}
+
+export default EditComponent;
