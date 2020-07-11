ae721715422e8b5459b742511c8651ca8956e56b
InesAzougado <57488512+InesAzougado@users.noreply.github.com>
2020-07-11 14:40:17 +0100

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -93,7 +93,7 @@ function App() {
               </div>
               <div class="fundobranco col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <div class="login">
-                <Link to="/login"><img src="https://i.ibb.co/DtrFdt4/login.png" width="20px" alt="Login"></img> Logout </Link>
+                <Link to="/logout"><img src="https://i.ibb.co/DtrFdt4/login.png" width="20px" alt="Logout"></img> Logout </Link>
                 </div>
               </div>
   
